81b617ea264aeebda021f470412ff1b4cec3bbc7
David Heinemeier Hansson <david@hey.com>
2025-06-01 11:56:03 +0200

    Add bash configuration

--- install/configurations.sh
+++ install/configurations.sh
@@ -5,5 +5,8 @@ rm -rf ~/.config/nvim/.git
 # Copy over Omarchy configs
 cp -R ~/.local/share/omarchy/config/* ~/.config/
 
+# Use default bashrc from Omarchy
+echo "source ~/.local/share/omarchy/default/bash/rc" >~/.bashrc
+
 # Turn off relative line numbers in nvim
 echo "vim.opt.relativenumber = false" >>~/.config/nvim/lua/config/options.lua
