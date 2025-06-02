c5ee230dafe665d78b65b116fb23cae3ead6b174
David Heinemeier Hansson <david@hey.com>
2025-06-02 10:26:04 +0200

    Stop the logout that happens under arch from this

--- install/docker.sh
+++ install/docker.sh
@@ -1,7 +1,7 @@
 yay -S --noconfirm docker
 
 # Give this user privileged Docker access
-sudo usermod -aG docker ${USER}
+# sudo usermod -aG docker ${USER}
 
 # Limit log size to avoid running out of disk
 echo '{"log-driver":"json-file","log-opts":{"max-size":"10m","max-file":"5"}}' | sudo tee /etc/docker/daemon.json
