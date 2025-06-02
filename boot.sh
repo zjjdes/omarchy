5a35a2c3bca4cdeaa127ea353435962360e4c57e
David Heinemeier Hansson <david@hey.com>
2025-06-02 10:40:10 +0200

    Dont need the splits for now

--- boot.sh
+++ boot.sh
@@ -17,11 +17,6 @@ sudo pacman -Sy --noconfirm --needed git >/dev/null
 echo "Cloning Omarchy..."
 rm -rf ~/.local/share/omarchy/
 git clone https://github.com/basecamp/omarchy.git ~/.local/share/omarchy >/dev/null
-if [[ $OMARCHY_REF != "master" ]]; then
-  cd ~/.local/share/omarchy
-  git fetch origin "${OMARCHY_REF:-stable}" && git checkout "${OMARCHY_REF:-stable}"
-  cd -
-fi
 
 echo "Installation starting..."
 source ~/.local/share/omarchy/install.sh
