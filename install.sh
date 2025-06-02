abe39314d9acc0713d5c28c200656097f943b9d0
David Heinemeier Hansson <david@hey.com>
2025-06-02 10:31:27 +0200

    No need to bail early

--- install.sh
+++ install.sh
@@ -1,12 +1,4 @@
-# Exit immediately if a command exits with a non-zero status
-set -e
-
-# Give people a chance to retry running the installation
-trap 'echo "Omarchy installation failed! You can retry by running: source ~/.local/share/omarchy/install.sh"' ERR
-
 # Install everything
 for f in ~/.local/share/omarchy/install/*.sh; do source "$f"; done
 
-# Reboot
-echo "Rebooting for all settings to apply..."
-reboot
+echo "Reboot for all settings to apply..."
