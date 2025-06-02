903f01ecde80f66a6213b2f1394990d235cfd474
David Heinemeier Hansson <david@hey.com>
2025-06-02 08:28:59 +0200

    Not ready for themeing yet

---
+++ install/ssdm.sh
@@ -0,0 +1,25 @@
+sudo pacman -Sy \
+  sddm-kcm \
+  qt6-base qt6-svg qt6-virtualkeyboard qt6-multimedia
+
+sudo tee /usr/share/wayland-sessions/hyprland.desktop >/dev/null <<EOF
+[Desktop Entry]
+Name=Hyprland
+Comment=An intelligent dynamic tiling Wayland compositor
+Exec=Hyprland
+Type=Application
+DesktopNames=Hyprland
+EOF
+
+# sudo tee /etc/ssdm.conf >/dev/null <<EOF
+# [Theme]
+# Current=This
+# EOF
+
+sudo tee /etc/ssdm.conf.d/virtualkbd.conf >/dev/null <<EOF
+[General]
+InputMethod=qtvirtualkeyboard
+EOF
+
+sudo systemctl enable sddm.service
+sudo systemctl start sddm.service
