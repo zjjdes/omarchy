8cebe3331899535a3a6b4bec7f90c5fa6b4445c8
David Heinemeier Hansson <david@hey.com>
2025-06-02 10:35:35 +0200

    Only install if needed

--- boot.sh
+++ boot.sh
@@ -12,7 +12,7 @@ echo -e "$ascii_art"
 echo "=> Omarchy is for fresh Arch installations only!"
 echo -e "\nBegin installation (or abort with ctrl+c)..."
 
-sudo pacman -Sy --noconfirm git >/dev/null
+sudo pacman -Sy --noconfirm --needed git >/dev/null
 
 echo "Cloning Omarchy..."
 rm -rf ~/.local/share/omarchy/
