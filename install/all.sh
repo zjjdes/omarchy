874c39f1800844aacae194ebf31d66745f5fa51f
David Heinemeier Hansson <david@hey.com>
2025-06-02 09:22:13 +0200

    Install ssdm by default

--- install/all.sh
+++ install/all.sh
@@ -5,4 +5,5 @@ source ~/.local/share/omarchy/install/desktop.sh
 source ~/.local/share/omarchy/install/terminal.sh
 source ~/.local/share/omarchy/install/development.sh
 source ~/.local/share/omarchy/install/docker.sh
+source ~/.local/share/omarchy/install/ssdm.sh
 source ~/.local/share/omarchy/install/configurations.sh
