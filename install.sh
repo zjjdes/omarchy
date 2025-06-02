df42ec1123ede5d475dcb2df8c80ccc979a4db5b
David Heinemeier Hansson <david@hey.com>
2025-06-02 21:24:45 +0200

    Ask for extras

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 # Install everything
 for f in ~/.local/share/omarchy/install/*.sh; do source "$f"; done
 
+gum confirm "Install all extra desktop apps?" && source ~/.local/share/omarchy/extras.sh
 gum confirm "Reboot to apply all settings?" && reboot
