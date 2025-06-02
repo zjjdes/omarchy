8af9ab91ce2adb872ea86bffd9547150e314fec3
David Heinemeier Hansson <david@hey.com>
2025-06-02 09:36:31 +0200

    Set the git aliases

--- install/development.sh
+++ install/development.sh
@@ -9,3 +9,10 @@ rm -rf ~/.config/nvim/.git
 
 # Turn off relative line numbers in nvim
 echo "vim.opt.relativenumber = false" >>~/.config/nvim/lua/config/options.lua
+
+# Set common git aliases
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.ci commit
+git config --global alias.st status
+git config --global pull.rebase true
