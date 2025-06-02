09f00f200b540c15a9a52fc040d9944a4bd77f6f
David Heinemeier Hansson <david@hey.com>
2025-06-02 10:43:08 +0200

    No need for double talk

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
 # Install everything
 for f in ~/.local/share/omarchy/install/*.sh; do source "$f"; done
 
-echo "Reboot for all settings to apply..."
 gum confirm "Reboot to apply all settings?" && reboot
