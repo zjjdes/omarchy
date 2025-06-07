f1d2be23ea80ed9f2c45dafc220efe9db645f76c
David Heinemeier Hansson <david@hey.com>
2025-06-07 21:25:19 +0200

    Ensure we can control laptop powerprofile

--- install/desktop.sh
+++ install/desktop.sh
@@ -1,4 +1,4 @@
 yay -S --noconfirm --needed \
-  brightnessctl playerctl wpctl pamixer wireplumber \
+  brightnessctl playerctl wpctl pamixer wireplumber power-profiles-daemon \
   nautilus google-chrome vlc \
   noto-fonts-emoji
