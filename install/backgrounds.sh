a745ad08620a8eb683592878026cc6d930194db1
David Heinemeier Hansson <david@hey.com>
2025-06-09 11:59:21 +0200

    Change background setup to cycle

---
+++ install/backgrounds.sh
@@ -0,0 +1,2 @@
+gum spin --title "Downloading background image..." -- curl -sL -o ~/.local/share/backgrounds/Milad-Fakurian-Abstract-Purple-Blue.jpg https://images.unsplash.com/photo-1620207418302-439b387441b0
+gum spin --title "Downloading background image..." -- curl -sL -o ~/.local/share/backgrounds/Pawel-Czerwinski-Abstract-Purple-Blue.jpg https://images.unsplash.com/photo-1651870364199-fc5f9f46ac85
--- install/theme.sh
+++ install/theme.sh
@@ -1,5 +1,2 @@
 # Prefer dark mode everything
 gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
-
-# Download first background
-curl -sL -o ~/.local/share/backgrounds/Milad-Fakurian-Abstract-Purple-Blue.jpg https://unsplash.com/photos/u8Jn2rzYIps/download?ixid=M3wxMjA3fDB8MXxzZWFyY2h8Mnx8YWJzdHJhY3QlMjB3YWxscGFwZXJ8ZW58MHx8fHwxNzQ5Mjc5ODk1fDA
