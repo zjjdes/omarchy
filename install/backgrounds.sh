d9856acd5e411c42c856d099f30d8784333285b2
David Heinemeier Hansson <david@hey.com>
2025-06-09 12:11:13 +0200

    Add one more

--- install/backgrounds.sh
+++ install/backgrounds.sh
@@ -1,2 +1,4 @@
 gum spin --title "Downloading background image..." -- curl -sL -o ~/.local/share/backgrounds/Milad-Fakurian-Abstract-Purple-Blue.jpg https://images.unsplash.com/photo-1620207418302-439b387441b0
 gum spin --title "Downloading background image..." -- curl -sL -o ~/.local/share/backgrounds/Pawel-Czerwinski-Abstract-Purple-Blue.jpg https://images.unsplash.com/photo-1651870364199-fc5f9f46ac85
+
+gum spin --title "Downloading background image..." -- curl -sL -o ~/.local/share/backgrounds/Jr-Korpa-Abstract-Purple-Blue.jpg https://images.unsplash.com/flagged/photo-1567400358593-9e6382752ea2
