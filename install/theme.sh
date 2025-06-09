fde252d9830cbbd9e9c01f91e2da975a9b516bc9
David Heinemeier Hansson <david@hey.com>
2025-06-09 12:38:03 +0200

    Need kvantum to dark mode vlc

--- install/theme.sh
+++ install/theme.sh
@@ -1,2 +1,5 @@
+# Use dark mode for QT apps too (like VLC and kdenlive)
+sudo pacman -S --noconfirm kvantum-qt5
+
 # Prefer dark mode everything
 gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
