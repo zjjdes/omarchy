050b0f1fb496c2f94b9105eb5009b85b93298793
David Heinemeier Hansson <david@hey.com>
2025-06-11 14:06:26 +0200

    Use chrome unstable until fcitx5 bug on new windows is fixed in chromium v138

--- install/desktop.sh
+++ install/desktop.sh
@@ -2,5 +2,5 @@ yay -S --noconfirm --needed \
   brightnessctl playerctl wpctl pamixer wireplumber power-profiles-daemon \
   fcitx5 fcitx5-gtk fcitx5-qt fcitx5-configtool \
   nautilus gnome-calculator \
-  chromium vlc \
+  chromium google-chrome-unstable vlc \
   evince imv
