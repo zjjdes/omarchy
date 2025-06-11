75e539f98ac8697ede45ab1e3f0d7f11eaceebb6
David Heinemeier Hansson <david@hey.com>
2025-06-11 09:54:22 +0200

    Add gnome calculator as default app

--- install/desktop.sh
+++ install/desktop.sh
@@ -1,6 +1,7 @@
 yay -S --noconfirm --needed \
   brightnessctl playerctl wpctl pamixer wireplumber power-profiles-daemon \
   fcitx5 fcitx5-gtk fcitx5-qt fcitx5-configtool \
-  nautilus chromium vlc \
+  nautilus gnome-calculator \
+  chromium vlc \
   evince imv \
   noto-fonts-emoji
