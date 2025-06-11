7f9a740dd8d609d25e27940ac49f33f1452489ee
David Heinemeier Hansson <david@hey.com>
2025-06-11 14:00:22 +0200

    Add missing theme for dark mode

--- install/theme.sh
+++ install/theme.sh
@@ -2,4 +2,5 @@
 sudo pacman -S --noconfirm kvantum-qt5
 
 # Prefer dark mode everything
+sudo pacman -S --noconfirm gnome-themes-extra # Adds Adwaita-dark theme
 gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
