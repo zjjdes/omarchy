116cab3390d000e872e4d1c2a6efd3de91b2d95c
David Heinemeier Hansson <david@hey.com>
2025-06-16 14:55:54 +0200

    Add asdcontrol for Apple Display brightness

---
+++ install/adscontrol.sh
@@ -0,0 +1,11 @@
+# Install asdcontrol for controlling brightness on Apple Displays
+git clone https://github.com/nikosdion/asdcontrol.git /tmp/asdcontrol
+cd /tmp/asdcontrol
+make
+sudo make install
+cd -
+rm -rf /tmp/asdcontrol
+
+# Setup sudo-less controls
+echo "$USER ALL=(ALL) NOPASSWD: /usr/local/bin/asdcontrol" | sudo tee /etc/sudoers.d/asdcontrol
+sudo chmod 440 /etc/sudoers.d/asdcontrol
