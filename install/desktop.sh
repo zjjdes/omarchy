21a6d57de8ab597d138ba73e5d2ded5bbe6e0e3f
David Heinemeier Hansson <david@hey.com>
2025-06-16 13:22:30 +0200

    Add sushi for space previews in nautilus

--- install/desktop.sh
+++ install/desktop.sh
@@ -1,6 +1,6 @@
 yay -S --noconfirm --needed \
   brightnessctl playerctl wpctl pamixer wireplumber power-profiles-daemon blueberry \
   fcitx5 fcitx5-gtk fcitx5-qt fcitx5-configtool \
-  nautilus gnome-calculator \
+  nautilus sushi gnome-calculator \
   chromium google-chrome-dev vlc \
   evince imv
