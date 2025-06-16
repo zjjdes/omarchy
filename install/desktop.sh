34ceb013e9050041795faef590c74f505846ff02
David Heinemeier Hansson <david@hey.com>
2025-06-16 10:25:43 +0200

    Correct package name

--- install/desktop.sh
+++ install/desktop.sh
@@ -2,5 +2,5 @@ yay -S --noconfirm --needed \
   brightnessctl playerctl wpctl pamixer wireplumber power-profiles-daemon \
   fcitx5 fcitx5-gtk fcitx5-qt fcitx5-configtool \
   nautilus gnome-calculator \
-  chromium google-chrome-unstable vlc \
+  chromium google-chrome-dev vlc \
   evince imv
