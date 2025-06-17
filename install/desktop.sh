7b0da0f79f958fe8ec99e963f87e3d456de1b83e
David Heinemeier Hansson <david@hey.com>
2025-06-17 21:37:20 +0200

    Add clipboard persistence + manager

--- install/desktop.sh
+++ install/desktop.sh
@@ -1,6 +1,7 @@
 yay -S --noconfirm --needed \
   brightnessctl playerctl wpctl pamixer wireplumber power-profiles-daemon blueberry \
   fcitx5 fcitx5-gtk fcitx5-qt fcitx5-configtool \
+  wl-clip-persist clipse \
   nautilus sushi gnome-calculator \
   chromium google-chrome-dev vlc \
   evince imv
