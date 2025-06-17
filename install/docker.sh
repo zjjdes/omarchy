fdd2aaf4d1f84647ac08de04384313a0c681afdb
David Heinemeier Hansson <david@hey.com>
2025-06-17 09:57:24 +0200

    Try again adding user to docker group

--- install/docker.sh
+++ install/docker.sh
@@ -1,10 +1,10 @@
 yay -S --noconfirm --needed docker
 
-# Give this user privileged Docker access
-# sudo usermod -aG docker ${USER}
-
 # Limit log size to avoid running out of disk
 echo '{"log-driver":"json-file","log-opts":{"max-size":"10m","max-file":"5"}}' | sudo tee /etc/docker/daemon.json
 
 # Start Docker automatically
 sudo systemctl enable docker
+
+# Give this user privileged Docker access
+sudo usermod -aG docker ${USER}
