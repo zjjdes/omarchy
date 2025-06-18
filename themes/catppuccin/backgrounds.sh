4e825cfb8b0e39e067eb8c7a65f68bca6e8c1e95
David Heinemeier Hansson <david@hey.com>
2025-06-18 21:41:56 +0200

    Fix dir checks

--- themes/catppuccin/backgrounds.sh
+++ themes/catppuccin/backgrounds.sh
@@ -1,6 +1,6 @@
 BACKGROUNDS_DIR=~/.config/omarchy/backgrounds/catppucin
 
-if [[ -z $BACKGROUNDS_DIR ]]; then
+if [[ ! -d $BACKGROUNDS_DIR ]]; then
   mkdir -p $BACKGROUNDS_DIR
   gum spin --title "Downloading background image..." -- curl -sL -o $BACKGROUNDS_DIR/1-catppuccin.png https://github.com/basecamp/omakub/raw/refs/heads/master/themes/catppuccin/background.png
 fi
