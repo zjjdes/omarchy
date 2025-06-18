a3efe87b1ad2a0e31651dbf5e8e50f5e82dbcc22
David Heinemeier Hansson <david@hey.com>
2025-06-18 21:31:46 +0200

    Catpuccin background

--- themes/catppuccin/backgrounds.sh
+++ themes/catppuccin/backgrounds.sh
@@ -2,7 +2,5 @@ BACKGROUNDS_DIR=~/.config/omarchy/backgrounds/catppucin
 
 if [[ -z $BACKGROUNDS_DIR ]]; then
   mkdir -p $BACKGROUNDS_DIR
-  gum spin --title "Downloading background image..." -- curl -sL -o $BACKGROUNDS_DIR/1-Milad-Fakurian-Abstract-Purple-Blue.jpg https://images.unsplash.com/photo-1620207418302-439b387441b0
-  gum spin --title "Downloading background image..." -- curl -sL -o $BACKGROUNDS_DIR/2-Pawel-Czerwinski-Abstract-Purple-Blue.jpg https://images.unsplash.com/photo-1651870364199-fc5f9f46ac85
-  gum spin --title "Downloading background image..." -- curl -sL -o $BACKGROUNDS_DIR/3-Jr-Korpa-Abstract-Purple-Blue.jpg https://images.unsplash.com/flagged/photo-1567400358593-9e6382752ea2
+  gum spin --title "Downloading background image..." -- curl -sL -o $BACKGROUNDS_DIR/1-catppuccin.png https://github.com/basecamp/omakub/raw/refs/heads/master/themes/catppuccin/background.png
 fi
