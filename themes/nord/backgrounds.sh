4617eb458bec1c98c6c03647bcade1657102c5f6
David Heinemeier Hansson <david@hey.com>
2025-06-19 03:04:02 +0200

    Fix folder name

--- themes/nord/backgrounds.sh
+++ themes/nord/backgrounds.sh
@@ -2,5 +2,5 @@ BACKGROUNDS_DIR=~/.config/omarchy/backgrounds/nord
 
 if [[ ! -d $BACKGROUNDS_DIR ]]; then
   mkdir -p $BACKGROUNDS_DIR
-  gum spin --title "Downloading background image..." -- curl -sL -o $BACKGROUNDS_DIR/1-nord.png https://github.com/basecamp/omakub/raw/refs/heads/master/themes/catppuccin/background.png
+  gum spin --title "Downloading background image..." -- curl -sL -o $BACKGROUNDS_DIR/1-nord.png https://github.com/basecamp/omakub/raw/refs/heads/master/themes/nord/background.png
 fi
