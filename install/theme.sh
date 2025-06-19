72af673ffca743a6a66d3249b03458f34010557b
David Heinemeier Hansson <david@hey.com>
2025-06-19 08:10:36 -0400

    Use current directory for gathering

--- install/theme.sh
+++ install/theme.sh
@@ -10,11 +10,12 @@ mkdir -p ~/.config/omarchy/themes
 for f in ~/.local/share/omarchy/themes/*; do ln -s "$f" ~/.config/omarchy/themes/; done
 
 # Set initial theme
-ln -snf ~/.config/omarchy/themes/tokyo-night ~/.config/omarchy/current-theme
+mkdir -p ~/.config/omarchy/current
+ln -snf ~/.config/omarchy/themes/tokyo-night ~/.config/omarchy/current/theme
 source ~/.local/share/omarchy/themes/tokyo-night/backgrounds.sh
-ln -snf ~/.config/omarchy/backgrounds/tokyo-night ~/.config/omarchy/current-backgrounds
+ln -snf ~/.config/omarchy/backgrounds/tokyo-night ~/.config/omarchy/current/backgrounds
 
 # Set specific app links for current theme
-ln -snf ~/.config/omarchy/current-theme/wofi.css ~/.config/wofi/style.css
-ln -snf ~/.config/omarchy/current-theme/neovim.lua ~/.config/nvim/lua/plugins/theme.lua
-ln -snf ~/.config/omarchy/current-theme/btop.theme ~/.config/btop/themes/current.theme
+ln -snf ~/.config/omarchy/current/theme/wofi.css ~/.config/wofi/style.css
+ln -snf ~/.config/omarchy/current/theme/neovim.lua ~/.config/nvim/lua/plugins/theme.lua
+ln -snf ~/.config/omarchy/current/theme/btop.theme ~/.config/btop/themes/current.theme
