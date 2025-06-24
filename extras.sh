233ce41131111fc784b18bce732d3aa4170e4d4c
David Heinemeier Hansson <david@hey.com>
2025-06-24 09:37:10 -0700

    Add zoom by default

--- extras.sh
+++ extras.sh
@@ -1,4 +1,4 @@
 yay -S --noconfirm --needed \
-  signal-desktop spotify 1password dropbox-cli \
+  signal-desktop spotify 1password dropbox-cli zoom \
   obsidian typora libreoffice obs-studio kdenlive \
   pinta xournalpp
