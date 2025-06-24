c1c3b23fbd9faaa0680a191ffcbfcb2f8bab38da
David Heinemeier Hansson <david@hey.com>
2025-06-24 12:06:27 -0700

    Man by default please

--- install/3-terminal.sh
+++ install/3-terminal.sh
@@ -2,5 +2,5 @@ yay -S --noconfirm --needed \
   wget curl unzip \
   eza fzf ripgrep zoxide \
   wl-clipboard fastfetch btop \
-  tldr less whois plocate \
+  man tldr less whois plocate \
   alacritty
