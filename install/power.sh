70ed06924973c72bacc22342dca5bce15e215a4e
David Heinemeier Hansson <david@hey.com>
2025-06-24 09:36:36 -0700

    Explain purpose

--- install/power.sh
+++ install/power.sh
@@ -1,8 +1,11 @@
+# Setting the performance profile can make a big difference. By default, most systems seem to start in balanced mode,
+# even if they're not running off a battery. So let's make sure that's changed to performance.
 yay -S --noconfirm power-profiles-daemon
 
 if ls /sys/class/power_supply/BAT* &>/dev/null; then
-  # This computer runs on a battery, leave powerprofile at Balanced
+  # This computer runs on a battery
+  powerprofilesctl set balanced
 else
-  # Always use maximum power when using a desktop machine
+  # This computer runs on power outlet
   powerprofilesctl set performance
 fi
