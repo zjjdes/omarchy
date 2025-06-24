2006d53340b424a25507c0a5a3c44236bfa601e8
David Heinemeier Hansson <david@hey.com>
2025-06-24 12:17:05 -0700

    Make sure themes directory is there

--- install/theme.sh
+++ install/theme.sh
@@ -18,6 +18,7 @@ ln -snf ~/.config/omarchy/backgrounds/tokyo-night ~/.config/omarchy/current/back
 # Set specific app links for current theme
 ln -snf ~/.config/omarchy/current/theme/wofi.css ~/.config/wofi/style.css
 ln -snf ~/.config/omarchy/current/theme/neovim.lua ~/.config/nvim/lua/plugins/theme.lua
+mkdir -p ~/.config/btop/themes
 ln -snf ~/.config/omarchy/current/theme/btop.theme ~/.config/btop/themes/current.theme
 mkdir -p ~/.config/mako
 ln -snf ~/.config/omarchy/current/theme/mako.ini ~/.config/mako/config
