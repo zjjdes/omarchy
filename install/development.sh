c1c354b066895eeb4f0158375ec6a5304b6a9c36
David Heinemeier Hansson <david@hey.com>
2025-06-24 15:29:44 -0700

    Dont need to include the clients by default

--- install/development.sh
+++ install/development.sh
@@ -1,6 +1,6 @@
 yay -S --noconfirm --needed \
   cargo clang llvm mise \
   imagemagick \
-  mariadb-libs mariadb-clients postgresql-libs \
+  mariadb-libs postgresql-libs \
   github-cli \
   lazygit lazydocker
