e03d02836e9d16c83869a41b419856486e5bcb09
David Heinemeier Hansson <david@hey.com>
2025-06-24 15:37:23 -0700

    No -g needed

--- install/ruby.sh
+++ install/ruby.sh
@@ -2,7 +2,7 @@ if ! command -v ruby &>/dev/null; then
   # Install Ruby using gcc-14 for compatibility
   yay -S --noconfirm --needed gcc14
   mise settings set ruby.ruby_build_opts "CC=gcc-14 CXX=g++-14"
-  mise install -g ruby
+  mise install ruby
 
   # Trust .ruby-version
   mise settings add idiomatic_version_file_enable_tools ruby
