4e4854654bb8116f037ced3f10315cb980c1d87d
David Heinemeier Hansson <david@hey.com>
2025-06-25 12:44:44 -0700

    Ensure file picker is also using dark mode

--- install/theme.sh
+++ install/theme.sh
@@ -3,6 +3,7 @@ sudo pacman -S --noconfirm kvantum-qt5
 
 # Prefer dark mode everything
 sudo pacman -S --noconfirm gnome-themes-extra # Adds Adwaita-dark theme
+gsettings set org.gnome.desktop.interface gtk-theme "Adwaita-dark"
 gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
 
 # Setup theme links
