b51a4efedb099d04c513ddde837585de85faba21
David Heinemeier Hansson <david@hey.com>
2025-06-25 12:04:50 -0700

    Add X as well

--- install/webapps.sh
+++ install/webapps.sh
@@ -4,3 +4,4 @@ web2app "Google Contacts" https://contacts.google.com/ https://cdn.jsdelivr.net/
 web2app "ChatGPT" https://chatgpt.com/ https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/chatgpt.png
 web2app "Youtube" https://youtube.com/ https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/youtube.png
 web2app "GitHub" https://github.com/ https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/github-light.png
+web2app "X" https://x.com/ https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/x-light.png
