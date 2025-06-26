1c1dd531d4b98e08f897709a5cf002f6fb075b63
David Heinemeier Hansson <david@hey.com>
2025-06-25 18:46:45 -0700

    Open all video files with VLC

--- install/mimetypes.sh
+++ install/mimetypes.sh
@@ -10,3 +10,20 @@ xdg-mime default imv.desktop image/tiff
 
 # Open PDFs with the Document Viewer
 xdg-mime default org.gnome.Evince.desktop application/pdf
+
+# Open video files with VLC
+xdg-mime default vlc.desktop video/mp4
+xdg-mime default vlc.desktop video/x-msvideo
+xdg-mime default vlc.desktop video/x-matroska
+xdg-mime default vlc.desktop video/x-flv
+xdg-mime default vlc.desktop video/x-ms-wmv
+xdg-mime default vlc.desktop video/mpeg
+xdg-mime default vlc.desktop video/ogg
+xdg-mime default vlc.desktop video/webm
+xdg-mime default vlc.desktop video/quicktime
+xdg-mime default vlc.desktop video/3gpp
+xdg-mime default vlc.desktop video/3gpp2
+xdg-mime default vlc.desktop video/x-ms-asf
+xdg-mime default vlc.desktop video/x-ogm+ogg
+xdg-mime default vlc.desktop video/x-theora+ogg
+xdg-mime default vlc.desktop application/ogg
