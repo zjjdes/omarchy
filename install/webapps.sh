464d9c8fa2258f5881d0d58c2616d7674fbe26b2
David Heinemeier Hansson <david@hey.com>
2025-06-25 19:57:08 -0700

    Make sure the web2app function is available for these

--- install/webapps.sh
+++ install/webapps.sh
@@ -1,3 +1,4 @@
+source ~/.local/share/omarchy/default/bash/functions
 web2app "WhatsApp" https://web.whatsapp.com/ https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/whatsapp.png
 web2app "Google Photos" https://photos.google.com/ https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/google-photos.png
 web2app "Google Contacts" https://contacts.google.com/ https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/google-contacts.png
