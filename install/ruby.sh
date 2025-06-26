9d9ac082751bb701604d586e7590f4b5e9efc853
David Heinemeier Hansson <david@hey.com>
2025-06-25 19:41:48 -0700

    Activate Ruby for use

--- install/ruby.sh
+++ install/ruby.sh
@@ -3,6 +3,7 @@ if ! command -v ruby &>/dev/null; then
   yay -S --noconfirm --needed gcc14
   mise settings set ruby.ruby_build_opts "CC=gcc-14 CXX=g++-14"
   mise install ruby
+  mise use ruby -g
 
   # Trust .ruby-version
   mise settings add idiomatic_version_file_enable_tools ruby
