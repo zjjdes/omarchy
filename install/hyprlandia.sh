fbd85990d07f2fe3aef7346eee77a716b2fc93e7
Kn0ax <me@kn0ax.com>
2025-06-29 22:19:36 +0200

    [not tested] nvidia support
    this commit tries to add nvidia suppport.
    it's not tested yet.

--- install/hyprlandia.sh
+++ install/hyprlandia.sh
@@ -3,5 +3,20 @@ yay -S --noconfirm --needed \
   wofi waybar mako swaybg \
   xdg-desktop-portal-hyprland xdg-desktop-portal-gtk
 
+# Checks if nvidia gpu exist
+gpu_info=$(lspci | grep -i 'nvidia')
+if [ -n "$gpu_info" ]; then
+    # Ask user if they want to install NVIDIA drivers
+    if [ -t 0 ]; then
+        read -p "NVIDIA GPU detected. Do you want to install NVIDIA drivers? (y/N) " install_nvidia
+        if [[ "$install_nvidia" =~ ^[yY](es)?$ ]]; then
+            source ./nvidia
+        fi
+    else
+        # Non-interactive mode - run nvidia script
+        source ./nvidia
+    fi
+fi
+
 # Start Hyprland on first session
 echo "[[ -z \$DISPLAY && \$(tty) == /dev/tty1 ]] && exec Hyprland" >~/.bash_profile
