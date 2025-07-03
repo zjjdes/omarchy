2c1c7550870327ba0a39ac8e20806cc1a1a62019
David Heinemeier Hansson <david@hey.com>
2025-07-02 19:27:07 -0700

    Kill the fd alias from omakub and just install fd
    Closes #32

--- install/3-terminal.sh
+++ install/3-terminal.sh
@@ -1,6 +1,6 @@
 yay -S --noconfirm --needed \
   wget curl unzip inetutils \
-  eza fzf ripgrep zoxide bat \
+  fd eza fzf ripgrep zoxide bat \
   wl-clipboard fastfetch btop \
   man tldr less whois plocate \
   alacritty
