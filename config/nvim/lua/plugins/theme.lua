609c8ef84e46fb0abd6eb02301c89aef26e466c6
David Heinemeier Hansson <david@hey.com>
2025-07-04 11:49:09 -0700

    Revert "Merge pull request #37 from prertik/nvim-switch-to-current-theme"
    This reverts commit 79a8d34cc5dedfcb9830bb4d218ee9d6486cd597, reversing
    changes made to d05020b7c17624cb4e2bd8debf319d8172da3bea.

--- config/nvim/lua/plugins/theme.lua
+++ config/nvim/lua/plugins/theme.lua
@@ -1,8 +1,4 @@
-local uv = vim.uv
-local file_path = vim.fn.expand("~/.config/omarchy/current/theme/neovim.lua")
-
--- Default configuration
-local default = {
+return {
 	{
 		"LazyVim/LazyVim",
 		opts = {
@@ -10,13 +6,3 @@ local default = {
 		},
 	},
 }
-
--- Try to load custom theme, fallback to default
-if uv.fs_stat(file_path) then
-	local success, result = pcall(dofile, file_path)
-	if success and type(result) == "table" then
-		return result
-	end
-end
-
-return default
