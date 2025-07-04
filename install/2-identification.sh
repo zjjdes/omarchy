fcaf61162c371f1b5b7b9f8387247ebb5ceea70e
David Heinemeier Hansson <david@hey.com>
2025-07-04 16:20:30 -0700

    Won't be set in that shell

--- install/2-identification.sh
+++ install/2-identification.sh
@@ -2,8 +2,6 @@
 yay -S --noconfirm --needed gum
 
 # Configure identification
-if [[ -z "${OMARCHY_USER_NAME//[[:space:]]/}${OMARCHY_USER_EMAIL//[[:space:]]/}" ]]; then
-  echo -e "\nEnter identification for git and autocomplete..."
-  export OMARCHY_USER_NAME=$(gum input --placeholder "Enter full name" --prompt "Name> ")
-  export OMARCHY_USER_EMAIL=$(gum input --placeholder "Enter email address" --prompt "Email> ")
-fi
+echo -e "\nEnter identification for git and autocomplete..."
+export OMARCHY_USER_NAME=$(gum input --placeholder "Enter full name" --prompt "Name> ")
+export OMARCHY_USER_EMAIL=$(gum input --placeholder "Enter email address" --prompt "Email> ")
