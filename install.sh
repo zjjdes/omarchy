98d758b50590bd7b54fe7cfafe2e64d6a172a496
David Heinemeier Hansson <david@hey.com>
2025-07-06 16:44:07 -0700

    Make it clear what installer we are running at a given moment

--- install.sh
+++ install.sh
@@ -5,7 +5,10 @@ set -e
 trap 'echo "Omarchy installation failed! You can retry by running: source ~/.local/share/omarchy/install.sh"' ERR
 
 # Install everything
-for f in ~/.local/share/omarchy/install/*.sh; do source "$f"; done
+for f in ~/.local/share/omarchy/install/*.sh; do
+  echo -e "\nRunning installer: $f"
+  source "$f"
+done
 
 # Ensure locate is up to date now that everything has been installed
 sudo updatedb
