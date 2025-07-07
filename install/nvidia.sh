d5fbab541f3f553c07884953969025f1e99182a0
David Heinemeier Hansson <david@hey.com>
2025-07-06 17:04:31 -0700

    Fix the grep target

--- install/nvidia.sh
+++ install/nvidia.sh
@@ -12,7 +12,7 @@
 if [ -n "$(lspci | grep -i 'nvidia')" ]; then
   # --- Driver Selection ---
   # Turing (16xx, 20xx), Ampere (30xx), Ada (40xx), and newer recommend the open-source kernel modules
-  if echo "$gpu_info" | grep -q -E "RTX [2-9][0-9]|GTX 16"; then
+  if echo "$(lspci | grep -i 'nvidia')" | grep -q -E "RTX [2-9][0-9]|GTX 16"; then
     NVIDIA_DRIVER_PACKAGE="nvidia-open-dkms"
   else
     NVIDIA_DRIVER_PACKAGE="nvidia-dkms"
