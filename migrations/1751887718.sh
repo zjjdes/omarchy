8939d636fb06ee0a8479257a7f4abdc5a9e895bf
David Heinemeier Hansson <david@hey.com>
2025-07-07 10:56:37 -0700

    Offer to refresh waybar to use Impala

--- migrations/1751887718.sh
+++ migrations/1751887718.sh
@@ -1,2 +1,3 @@
-echo "Install impala"
+echo "Install Impala as new wifi selection TUI"
 yay -S --noconfirm --needed impala
+gum confirm "Overwrite Waybar config to use Impala Wi-Fi selector in top bar?" && omarchy-refresh-waybar
