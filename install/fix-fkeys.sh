5c1d8cc2b3f5e5ea84ea620553935aa856e09bdf
David Heinemeier Hansson <david@hey.com>
2025-07-09 19:05:37 -0400

    Only run if not already configured

--- install/fix-fkeys.sh
+++ install/fix-fkeys.sh
@@ -1,2 +1,4 @@
-echo "options hid_apple fnmode=2" | sudo tee /etc/modprobe.d/hid_apple.conf
-sudo mkinitcpio -P
+if [[ ! -f /etc/modprobe.d/hid_apple.conf ]]; do
+  echo "options hid_apple fnmode=2" | sudo tee /etc/modprobe.d/hid_apple.conf
+  sudo mkinitcpio -P
+fi
