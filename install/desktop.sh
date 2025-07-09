9b418abed2787a3055b4a74dac770b98cd525626
David Heinemeier Hansson <david@hey.com>
2025-07-09 11:50:32 -0700

    Drop clipse since it would keep clear-text passwords from 1passwords in the buffer
    And there's no exclusion option!

--- install/desktop.sh
+++ install/desktop.sh
@@ -1,7 +1,7 @@
 yay -S --noconfirm --needed \
   brightnessctl playerctl pamixer pavucontrol wireplumber \
   fcitx5 fcitx5-gtk fcitx5-qt fcitx5-configtool \
-  wl-clip-persist clipse-bin \
+  wl-clip-persist \
   nautilus sushi ffmpegthumbnailer gnome-calculator \
   1password-beta 1password-cli gnome-keyring \
   chromium mpv \
