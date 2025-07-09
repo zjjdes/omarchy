31875282ec76dcfb79f7077736a969668fcfe21f
David Heinemeier Hansson <david@hey.com>
2025-07-09 19:14:10 -0400

    Then not do

--- install/fix-fkeys.sh
+++ install/fix-fkeys.sh
@@ -1,4 +1,4 @@
-if [[ ! -f /etc/modprobe.d/hid_apple.conf ]]; do
+if [[ ! -f /etc/modprobe.d/hid_apple.conf ]]; then
   echo "options hid_apple fnmode=2" | sudo tee /etc/modprobe.d/hid_apple.conf
   sudo mkinitcpio -P
 fi
