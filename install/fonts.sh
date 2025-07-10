52f7bd98c15b21cae498681d0700b8ecacb7f1a6
David Heinemeier Hansson <david@hey.com>
2025-07-10 15:50:51 -0700

    Make sure we have all of the Propo font styles

--- install/fonts.sh
+++ install/fonts.sh
@@ -15,6 +15,9 @@ if ! fc-list | grep -qi "CaskaydiaMono Nerd Font"; then
   cp CascadiaFont/CaskaydiaMonoNerdFont-Italic.ttf ~/.local/share/fonts
   cp CascadiaFont/CaskaydiaMonoNerdFont-BoldItalic.ttf ~/.local/share/fonts
   cp CascadiaFont/CaskaydiaMonoNerdFontPropo-Regular.ttf ~/.local/share/fonts
+  cp CascadiaFont/CaskaydiaMonoNerdFontPropo-Bold.ttf ~/.local/share/fonts
+  cp CascadiaFont/CaskaydiaMonoNerdFontPropo-Italic.ttf ~/.local/share/fonts
+  cp CascadiaFont/CaskaydiaMonoNerdFontPropo-BoldItalic.ttf ~/.local/share/fonts
   rm -rf CascadiaMono.zip CascadiaFont
   fc-cache
   cd -
