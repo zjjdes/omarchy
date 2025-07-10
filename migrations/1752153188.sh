9ff7fa715e716a04d3fae2483e0adc89071a8341
David Heinemeier Hansson <david@hey.com>
2025-07-10 15:04:01 -0700

    Force the copy, there's no recursion needed

--- migrations/1752153188.sh
+++ migrations/1752153188.sh
@@ -1,2 +1,2 @@
 echo "Migrate to the modular implementation of hyprlock"
-cp -R ~/.local/share/omarchy/config/hypr/hyprlock.conf ~/.config/hypr
+cp -f ~/.local/share/omarchy/config/hypr/hyprlock.conf ~/.config/hypr
