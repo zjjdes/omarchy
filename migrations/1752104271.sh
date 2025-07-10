6434fddb594b0440ac4bb1f5043da5af64a6dc6a
David Heinemeier Hansson <david@hey.com>
2025-07-09 20:45:17 -0400

    Explain migration

--- migrations/1752104271.sh
+++ migrations/1752104271.sh
@@ -1,3 +1,4 @@
+echo "Switching to polkit-gnome for better fingerprint authentication compatibility"
 if ! command -v /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &>/dev/null; then
   yay -S --noconfirm --needed polkit-gnome
   systemctl --user stop hyprpolkitagent
