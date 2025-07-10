c314721007782269129e06cbc3825494e2896046
David Heinemeier Hansson <david@hey.com>
2025-07-09 18:01:56 -0700

    Rely on omarchy-refresh-waybar having its own confirm

--- migrations/1751887718.sh
+++ migrations/1751887718.sh
@@ -1,5 +1,6 @@
 echo "Install Impala as new wifi selection TUI"
 if ! command -v impala &>/dev/null; then
   yay -S --noconfirm --needed impala
-  gum confirm "Overwrite Waybar config to use Impala Wi-Fi selector in top bar?" && omarchy-refresh-waybar
+  echo "You need to update the Waybar config to use Impala Wi-Fi selector in top bar."
+  omarchy-refresh-waybar
 fi
