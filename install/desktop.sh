a64c0505ecb35ca7b02cc3d00429a79e56a8aeb3
David Heinemeier Hansson <david@hey.com>
2025-07-10 12:28:42 -0700

    Add a bare mode with almost no default applications beyond system basics

--- install/desktop.sh
+++ install/desktop.sh
@@ -1,9 +1,6 @@
 yay -S --noconfirm --needed \
   brightnessctl playerctl pamixer pavucontrol wireplumber \
-  fcitx5 fcitx5-gtk fcitx5-qt fcitx5-configtool \
-  wl-clip-persist \
-  nautilus sushi ffmpegthumbnailer gnome-calculator \
-  1password-beta 1password-cli gnome-keyring \
-  chromium mpv \
-  evince imv \
-  localsend-bin
+  fcitx5 fcitx5-gtk fcitx5-qt fcitx5-configtool wl-clip-persist \
+  nautilus sushi ffmpegthumbnailer \
+  mpv evince imv \
+  chromium
