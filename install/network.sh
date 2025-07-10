2b0805a66ddf44faedfc94a35a939bc00acadfc6
Tim Culverhouse <tim@timculverhouse.com>
2025-07-10 13:45:17 -0500

    install: explicitly install iwd
    When archinstall is run using an ethernet connection, iwd will not be
    installed. Explicitly install and enable the service

---
+++ install/network.sh
@@ -0,0 +1,6 @@
+# Install iwd explicitly if it wasn't included in archinstall
+# This can happen if archinstall used ethernet
+if ! command -v iwd &>/dev/null; then
+  yay -S --noconfirm --needed iwd
+  sudo systemctl enable --now iwd.service
+fi
