b25a60395a4f9095e500e01f60881eb57d909cca
David Heinemeier Hansson <david@hey.com>
2025-07-09 19:00:20 -0700

    All protections are already in there

--- migrations/1752091783.sh
+++ migrations/1752091783.sh
@@ -1,5 +1,2 @@
 echo "Install Plymouth splash screen"
-if ! command -v plymouth &>/dev/null; then
-  yay -S --noconfirm --needed plymouth
-  bash "$HOME/.local/share/omarchy/install/plymouth.sh"
-fi
+source "$HOME/.local/share/omarchy/install/plymouth.sh"
