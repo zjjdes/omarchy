cbc64ad09f52db7ebc5678048c9ebdc751294946
David Heinemeier Hansson <david@hey.com>
2025-07-10 16:08:46 -0700

    Switch Basecamp + HEY to use the regular web2app

--- install/webapps.sh
+++ install/webapps.sh
@@ -1,5 +1,7 @@
 if [ -z "$OMARCHY_BARE" ]; then
   source ~/.local/share/omarchy/default/bash/functions
+  web2app "HEY" https://app.hey.com https://www.hey.com/assets/images/general/hey.png
+  web2app "Basecamp" https://launchpad.37signals.com https://basecamp.com/assets/images/general/basecamp.png
   web2app "WhatsApp" https://web.whatsapp.com/ https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/whatsapp.png
   web2app "Google Photos" https://photos.google.com/ https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/google-photos.png
   web2app "Google Contacts" https://contacts.google.com/ https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/google-contacts.png
