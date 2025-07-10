df8cdf7c64662af53d202b8a4a863e3d7bd61ffb
David Heinemeier Hansson <david@hey.com>
2025-07-10 16:46:17 -0700

    Reset xdg settings with the new .desktop file

--- migrations/1752188554.sh
+++ migrations/1752188554.sh
@@ -1,2 +1,5 @@
 echo "Update chromium.desktop to ensure we are always using wayland"
 cp ~/.local/share/omarchy/applications/chromium.desktop ~/.local/share/applications/
+xdg-settings set default-web-browser chromium.desktop
+xdg-mime default chromium.desktop x-scheme-handler/http
+xdg-mime default chromium.desktop x-scheme-handler/https
