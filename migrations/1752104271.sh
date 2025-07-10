5684aa80d83dd384564bd430c88a22445d27ea05
David Heinemeier Hansson <david@hey.com>
2025-07-09 20:30:16 -0400

    Make sure to run polkit-gnome after migrating to it

--- migrations/1752104271.sh
+++ migrations/1752104271.sh
@@ -2,3 +2,4 @@ yay -S --noconfirm --needed polkit-gnome
 systemctl --user stop hyprpolkitagent
 systemctl --user disable hyprpolkitagent
 yay -Rns --noconfirm hyprpolkitagent
+setsid /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
