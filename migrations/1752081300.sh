e6016034e6e83480954701bf31144cb4163d51a2
David Heinemeier Hansson <david@hey.com>
2025-07-10 16:49:14 -0700

    No longer relevant since we moved to polkit-gnome

--- migrations/1752081300.sh
+++
@@ -1,9 +0,0 @@
-echo "Switching system authentication prompt to dark mode"
-if [[ ! -f ~/.config/systemd/user/hyprpolkitagent.service.d/override.conf ]]; then
-  source ~/.local/share/omarchy/install/hyprpolkitagent.sh
-
-  # Reload user systemd and restart service
-  systemctl --user daemon-reexec
-  systemctl --user daemon-reload
-  systemctl --user restart hyprpolkitagent
-fi
