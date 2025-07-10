929aafc17c03c4048af4a00b8dfea6b97a74686c
David Heinemeier Hansson <david@hey.com>
2025-07-09 20:31:50 -0400

    Guard against running it twice

--- migrations/1752104271.sh
+++ migrations/1752104271.sh
@@ -1,5 +1,7 @@
-yay -S --noconfirm --needed polkit-gnome
-systemctl --user stop hyprpolkitagent
-systemctl --user disable hyprpolkitagent
-yay -Rns --noconfirm hyprpolkitagent
-setsid /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+if ! command -v /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &>/dev/null; then
+  yay -S --noconfirm --needed polkit-gnome
+  systemctl --user stop hyprpolkitagent
+  systemctl --user disable hyprpolkitagent
+  yay -Rns --noconfirm hyprpolkitagent
+  setsid /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+fi
