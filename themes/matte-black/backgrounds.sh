a477b415933d412e48ee73effc57000b1b0b6118
David Heinemeier Hansson <david@hey.com>
2025-07-11 09:19:17 -0700

    Fix backgrounds path

--- themes/matte-black/backgrounds.sh
+++ themes/matte-black/backgrounds.sh
@@ -1,2 +1,2 @@
-mkdir -p "$BACKGROUNDS_DIR/matte-black"
+mkdir -p "~/.config/omarchy/backgrounds/matte-black"
 download_background_image "https://raw.githubusercontent.com/tahayvr/omarchy-matte-black/refs/heads/main/images/1-matte-black.jpg" "matte-black/1-matte-black.jpg"
