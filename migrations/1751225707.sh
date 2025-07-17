9a2d8852d39c2f836623e7c691317aed01dc621e
David Heinemeier Hansson <david@hey.com>
2025-07-16 21:13:36 -0700

    Kill waybar proper and restart on suspecion that SIGUSR2 is whats causing the stacking bug

--- migrations/1751225707.sh
+++ migrations/1751225707.sh
@@ -1,3 +1,4 @@
 echo "Fixing persistent workspaces in waybar config"
 sed -i 's/"persistent_workspaces":/"persistent-workspaces":/' ~/.config/waybar/config
-pkill -SIGUSR2 waybar
+pkill waybar
+setsid waybar >/dev/null 2>&1 &
