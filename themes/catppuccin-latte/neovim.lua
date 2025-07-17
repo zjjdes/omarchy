2bcff1662b14f55ed96033bbbb6e365baf691c29
Ryan Yogan <ryanyogan@gmail.com>
2025-07-17 18:50:58 -0500

    removes personal neovim configs

--- themes/catppuccin-latte/neovim.lua
+++ themes/catppuccin-latte/neovim.lua
@@ -6,30 +6,6 @@ return {
 		config = function()
 			require("catppuccin").setup({
 				flavour = "latte", -- other options: "mocha", "frappe", "macchiato"
-				integrations = {
-					cmp = true,
-					gitsigns = true,
-					harpoon = true,
-					illuminate = true,
-					lsp_trouble = true,
-					markdown = true,
-					mason = true,
-					native_lsp = {
-						enabled = true,
-						underlines = {
-							errors = { "undercurl" },
-							hints = { "undercurl" },
-							warnings = { "undercurl" },
-							information = { "undercurl" },
-						},
-					},
-					navic = { enabled = true },
-					neotree = true,
-					notify = true,
-					telescope = true,
-					treesitter = true,
-					which_key = true,
-				},
 			})
 			vim.cmd.colorscheme("catppuccin-latte")
 		end,
