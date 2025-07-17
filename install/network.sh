51c2437f2a605b63993ee10753154aa6af2ec498
tobiloba ogundiyan <apprentice_py@icloud.com>
2025-07-17 18:57:37 -0400

    fix broken iwd check by validating iwctl instead

--- install/network.sh
+++ install/network.sh
@@ -1,6 +1,6 @@
 # Install iwd explicitly if it wasn't included in archinstall
 # This can happen if archinstall used ethernet
-if ! command -v iwd &>/dev/null; then
+if ! command -v iwctl &>/dev/null; then
   yay -S --noconfirm --needed iwd
   sudo systemctl enable --now iwd.service
 fi
