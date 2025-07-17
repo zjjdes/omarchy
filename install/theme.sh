e2c8e3ccfebddb1c2febc5af9b49061331eae3f7
David Heinemeier Hansson <david@hey.com>
2025-07-17 15:16:37 -0700

    Pull backgrounds into the repo to simplify setup and theme selection
    This was too complicated for something too remote

--- install/theme.sh
+++ install/theme.sh
@@ -15,13 +15,13 @@ for f in ~/.local/share/omarchy/themes/*; do ln -s "$f" ~/.config/omarchy/themes
 # Set initial theme
 mkdir -p ~/.config/omarchy/current
 ln -snf ~/.config/omarchy/themes/tokyo-night ~/.config/omarchy/current/theme
-source ~/.local/share/omarchy/themes/tokyo-night/backgrounds.sh
-ln -snf ~/.config/omarchy/backgrounds/tokyo-night ~/.config/omarchy/current/backgrounds
 ln -snf ~/.config/omarchy/current/backgrounds/1-Pawel-Czerwinski-Abstract-Purple-Blue.jpg ~/.config/omarchy/current/background
 
 # Set specific app links for current theme
 ln -snf ~/.config/omarchy/current/theme/neovim.lua ~/.config/nvim/lua/plugins/theme.lua
+
 mkdir -p ~/.config/btop/themes
 ln -snf ~/.config/omarchy/current/theme/btop.theme ~/.config/btop/themes/current.theme
+
 mkdir -p ~/.config/mako
 ln -snf ~/.config/omarchy/current/theme/mako.ini ~/.config/mako/config
