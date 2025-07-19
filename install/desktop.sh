2e874adb503ac6efcc0db6686c87b93505a4eac1
David Heinemeier Hansson <david@hey.com>
2025-07-18 20:55:28 -0700

    Add simple screen recordings (#235)
    * Add simple screen recording tool
    
    * Need slurp too

--- install/desktop.sh
+++ install/desktop.sh
@@ -4,5 +4,6 @@ yay -S --noconfirm --needed \
   brightnessctl playerctl pamixer wiremix wireplumber \
   fcitx5 fcitx5-gtk fcitx5-qt fcitx5-configtool wl-clip-persist \
   nautilus sushi ffmpegthumbnailer \
+  slurp wl-screenrec \
   mpv evince imv \
   chromium
---
+++ migrations/1752885858.sh
@@ -0,0 +1,2 @@
+echo "Install slurp + wl-screenrec for new ALT+PrintScreen screen recorder"
+yay -S --noconfirm --needed slurp wl-screenrec
