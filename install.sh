68a188d299a6edb87802abaf05a1883cdae1b7c9
David Heinemeier Hansson <david@hey.com>
2025-07-18 17:17:42 -0700

    Offer community help link

--- install.sh
+++ install.sh
@@ -2,7 +2,11 @@
 set -e
 
 # Give people a chance to retry running the installation
-trap 'echo "Omarchy installation failed! You can retry by running: source ~/.local/share/omarchy/install.sh"' ERR
+trap '
+  echo "Omarchy installation failed!"
+  echo "You can retry by running: source ~/.local/share/omarchy/install.sh"
+  echo "Get help from the community: https://discord.gg/tXFUdasqhY"
+' ERR
 
 # Install everything
 for f in ~/.local/share/omarchy/install/*.sh; do
