178f97a6e3334befe531ffbc7f87d193a09278fb
David Heinemeier Hansson <david@hey.com>
2025-07-18 23:33:08 -0500

    We don't need the GUI for fcitx5
    The fewer packages the better

--- install/desktop.sh
+++ install/desktop.sh
@@ -2,7 +2,7 @@
 
 yay -S --noconfirm --needed \
   brightnessctl playerctl pamixer wiremix wireplumber \
-  fcitx5 fcitx5-gtk fcitx5-qt fcitx5-configtool wl-clip-persist \
+  fcitx5 fcitx5-gtk fcitx5-qt wl-clip-persist \
   nautilus sushi ffmpegthumbnailer \
   slurp wl-screenrec \
   mpv evince imv \
