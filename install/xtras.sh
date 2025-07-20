81727a20cee24f20e38d17a4893c8b61b5cac8f5
David Heinemeier Hansson <david@hey.com>
2025-07-19 22:42:30 -0500

    Install everything needed for full Dropbox + split out other potentially flaky installs

--- install/xtras.sh
+++ install/xtras.sh
@@ -4,8 +4,7 @@ if [ -z "$OMARCHY_BARE" ]; then
   yay -S --noconfirm --needed \
     gnome-calculator gnome-keyring signal-desktop \
     obsidian-bin libreoffice obs-studio kdenlive \
-    xournalpp localsend-bin \
-    libappindicator-gtk3
+    xournalpp localsend-bin
 
   yay -S --noconfirm --needed pinta ||
     echo -e "\e[31mFailed to install Pinta. Continuing without!\e[0m"
@@ -13,8 +12,14 @@ if [ -z "$OMARCHY_BARE" ]; then
   yay -S --noconfirm --needed typora ||
     echo -e "\e[31mFailed to install Typora. Continuing without!\e[0m"
 
-  yay -S --noconfirm --needed spotify dropbox-cli 1password-beta 1password-cli ||
-    echo -e "\e[31mFailed to install Spotify, Dropbox, and/or 1password because of a keyserver problem. Continuing without!\e[0m"
+  yay -S --noconfirm --needed dropbox libappindicator-gtk3 python-gpgme nautilus-dropbox ||
+    echo -e "\e[31mFailed to install Dropbox. Continuing without!\e[0m"
+
+  yay -S --noconfirm --needed spotify ||
+    echo -e "\e[31mFailed to install Spotify (likely because of a keyserver problem). Continuing without!\e[0m"
+
+  yay -S --noconfirm --needed 1password-beta 1password-cli ||
+    echo -e "\e[31mFailed to install 1password (likely because of a keyserver problem). Continuing without!\e[0m"
 fi
 
 # Copy over Omarchy applications
