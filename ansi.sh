e8c23532b33728f3c8ae0c058198e2eea09cc43e
David Heinemeier Hansson <david@hey.com>
2025-07-21 16:22:28 -0400

    Reuse ansi art and clear install when we ask for input

--- ansi.sh
+++ ansi.sh
@@ -10,21 +10,5 @@ ansi_art=' ▄██████▄    ▄▄▄▄███▄▄▄▄      �
  ▀██████▀   ▀█   ███   █▀    ███    █▀    ███    ███ ████████▀    ███    █▀     ▀█████▀ 
                                           ███    ███                                    '
 
+clear
 echo -e "\n$ansi_art\n"
-
-pacman -Q git &>/dev/null || sudo pacman -Sy --noconfirm --needed git
-
-echo -e "\nCloning Omarchy..."
-rm -rf ~/.local/share/omarchy/
-git clone https://github.com/basecamp/omarchy.git ~/.local/share/omarchy >/dev/null
-
-# Use custom branch if instructed
-if [[ -n "$OMARCHY_REF" ]]; then
-  echo -e "\eUsing branch: $OMARCHY_REF"
-  cd ~/.local/share/omarchy
-  git fetch origin "${OMARCHY_REF}" && git checkout "${OMARCHY_REF}"
-  cd -
-fi
-
-echo -e "\nInstallation starting..."
-source ~/.local/share/omarchy/install.sh
