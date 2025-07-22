611668783bcbb26c6d38cdca0df7a94fae088ed8
David Heinemeier Hansson <david@hey.com>
2025-07-21 20:42:39 -0400

    Make sure we have jq for initial monitor setup

--- install/3-terminal.sh
+++ install/3-terminal.sh
@@ -2,7 +2,7 @@
 
 yay -S --noconfirm --needed \
   wget curl unzip inetutils impala \
-  fd eza fzf ripgrep zoxide bat \
+  fd eza fzf ripgrep zoxide bat jq \
   wl-clipboard fastfetch btop \
   man tldr less whois plocate bash-completion \
   alacritty
