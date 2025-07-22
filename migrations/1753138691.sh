8c0ac90fe47d602a8110319c6709f19fb5355e65
David Heinemeier Hansson <david@hey.com>
2025-07-22 19:17:41 -0400

    Remember to disown!

--- migrations/1753138691.sh
+++ migrations/1753138691.sh
@@ -2,5 +2,5 @@ echo "Install swayOSD to show volume status"
 
 if ! command -v swayosd &>/dev/null; then
   yay -S --noconfirm --needed swayosd
-  uwsm app -- swayosd-server &>/dev/null &
+  setsid uwsm app -- swayosd-server &>/dev/null &
 fi
