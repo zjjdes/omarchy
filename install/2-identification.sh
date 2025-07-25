6d3df204bc6aac677273e012d2b0d7b2e8b0dda8
David Heinemeier Hansson <david@hey.com>
2025-07-25 15:05:41 -0400

    Use terminal text effects for extra rizz

--- install/2-identification.sh
+++ install/2-identification.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 # Need gum to query for input
-yay -S --noconfirm --needed gum
+yay -S --noconfirm --needed gum python-terminaltexteffects
 
 # Configure identification
-source ~/.local/share/omarchy/ansi.sh
+clear
+tte -i ~/.local/share/omarchy/logo.txt --frame-rate 120 expand
 echo -e "\nEnter identification for git and autocomplete..."
 export OMARCHY_USER_NAME=$(gum input --placeholder "Enter full name" --prompt "Name> ")
 export OMARCHY_USER_EMAIL=$(gum input --placeholder "Enter email address" --prompt "Email> ")
