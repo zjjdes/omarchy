c4a08c3c25f6e53af81a9908b039fc7c95d1578e
David Heinemeier Hansson <david@hey.com>
2025-07-25 15:27:10 -0400

    Try to do another header

--- install/4-terminal.sh
+++ install/4-terminal.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+show_logo
+show_subtext "Let's install basic terminal tools..."
+
 yay -S --noconfirm --needed \
   wget curl unzip inetutils impala \
   fd eza fzf ripgrep zoxide bat jq \
