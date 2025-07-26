3c2559297d2b81f1b20ea417a64ef1a81171574e
Ryan Hughes <ryan@heyoodle.com>
2025-07-25 22:16:50 -0400

    Convert to yay and check for existence

--- install/desktop/theme.sh
+++ install/desktop/theme.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
 # Use dark mode for QT apps too (like kdenlive)
-sudo pacman -S --noconfirm kvantum-qt5
+if ! yay -Q kvantum-qt5 &>/dev/null; then
+  yay -S --noconfirm kvantum-qt5
+fi
 
 # Prefer dark mode everything
-sudo pacman -S --noconfirm gnome-themes-extra # Adds Adwaita-dark theme
+if ! yay -Q gnome-themes-extra &>/dev/null; then
+  yay -S --noconfirm gnome-themes-extra # Adds Adwaita-dark theme
+fi
+
 gsettings set org.gnome.desktop.interface gtk-theme "Adwaita-dark"
 gsettings set org.gnome.desktop.interface color-scheme "prefer-dark"
 
