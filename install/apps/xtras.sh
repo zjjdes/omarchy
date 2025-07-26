d9f9f5212e03621b9c60cb22e0dad4bd9bf91c28
David Heinemeier Hansson <david@hey.com>
2025-07-25 21:58:51 -0400

    Don't need that twice

--- install/apps/xtras.sh
+++ install/apps/xtras.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
 if [ -z "$OMARCHY_BARE" ]; then
-  show_logo
-  show_subtext "Installing default applications..."
-
   yay -S --noconfirm --needed \
     gnome-calculator gnome-keyring signal-desktop \
     obsidian-bin libreoffice obs-studio kdenlive \
