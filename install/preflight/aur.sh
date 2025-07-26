b4d75729d94840cef74f55bfab673ee084eaab4b
David Heinemeier Hansson <david@hey.com>
2025-07-26 15:53:53 -0400

    Don't add the keys twice

--- install/preflight/aur.sh
+++ install/preflight/aur.sh
@@ -3,7 +3,8 @@
 # Only add Chaotic-AUR if the architecture is x86_64 so ARM users can build the packages
 if [[ "$(uname -m)" == "x86_64" ]] && ! command -v yay &>/dev/null; then
   # Try installing Chaotic-AUR keyring and mirrorlist
-  if sudo pacman-key --recv-key 3056513887B78AEB &&
+  if ! pacman-key --list-keys 3056513887B78AEB >/dev/null 2>&1 &&
+    sudo pacman-key --recv-key 3056513887B78AEB &&
     sudo pacman-key --lsign-key 3056513887B78AEB &&
     sudo pacman -U --noconfirm 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' &&
     sudo pacman -U --noconfirm 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'; then
@@ -33,4 +34,6 @@ if ! command -v yay &>/dev/null; then
 fi
 
 # Add fun and color to the pacman installer
-sudo sed -i '/^\[options\]/a Color\nILoveCandy' /etc/pacman.conf
+if ! grep -q "ILoveCandy" /etc/pacman.conf; then
+  sudo sed -i '/^\[options\]/a Color\nILoveCandy' /etc/pacman.conf
+fi
