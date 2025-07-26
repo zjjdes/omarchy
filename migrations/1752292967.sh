07a16487c00e4a71122e7084c72fa76e0cdd44cc
David Heinemeier Hansson <david@hey.com>
2025-07-26 16:48:45 -0400

    Fix paths

--- migrations/1752292967.sh
+++ migrations/1752292967.sh
@@ -14,5 +14,5 @@ if ! command -v uwsm &>/dev/null; then
     sed -i 's/^GTK_IM_MODULE=fcitx$//' "$HOME/.config/environment.d/fcitx.conf"
   fi
 
-  source ~/.local/share/omarchy/install/login.sh
+  source ~/.local/share/omarchy/install/config/login.sh
 fi
