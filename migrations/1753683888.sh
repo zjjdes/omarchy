7589683cd11a49514304c997460916a365aed095
David Heinemeier Hansson <david@hey.com>
2025-07-28 09:58:51 +0200

    Adjust migration to check for whether fastfetch already has the change or not

--- migrations/1753683888.sh
+++ migrations/1753683888.sh
@@ -1,4 +1,5 @@
 echo "Adding Omarchy version info to fastfetch"
-if [ -d ~/.config/fastfetch ]; then
-    cp ~/.local/share/omarchy/config/fastfetch/config.jsonc ~/.config/fastfetch/
+if ! grep -q "omarchy" ~/.config/fastfetch/config.jsonc; then
+  cp ~/.local/share/omarchy/config/fastfetch/config.jsonc ~/.config/fastfetch/
 fi
+
