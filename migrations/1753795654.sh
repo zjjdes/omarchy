8c2f51c08c3eff3aa0c94da504999a393782c20a
David Heinemeier Hansson <david@hey.com>
2025-07-29 19:17:39 +0200

    Revert "Correct package name"
    This reverts commit 4c46c2208a57aea9caf6a9eb0ffa282ab8a2c87a.

--- migrations/1753795654.sh
+++ migrations/1753795654.sh
@@ -3,7 +3,7 @@ if [[ ! -f ~/.local/state/omarchy/bare.mode ]]; then
 
   if command -v spotify >/dev/null && ! command -v spotify_player >/dev/null; then
     if gum confirm "Replace Spotify GUI with TUI?"; then
-      yay -Sy --noconfirm spotify-player
+      yay -Sy --noconfirm spotify_player
       yay -Rns --noconfirm spotify
 
       sed -i -E 's|bind = (.+), exec, .*spotify|bind = \1, exec, $terminal -e spotify_player|' ~/.config/hypr/hyprland.conf
