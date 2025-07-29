d4f685902262317af140188da6ed550b30f2291e
David Heinemeier Hansson <david@hey.com>
2025-07-29 19:17:41 +0200

    Revert "Swap Spotify GUI to TUI"
    This reverts commit b09d2d68cd65167e7cff5f751caed1fa5ac38dba.

--- install/apps/xtras.sh
+++ install/apps/xtras.sh
@@ -4,10 +4,10 @@ if [ -z "$OMARCHY_BARE" ]; then
   yay -S --noconfirm --needed \
     gnome-calculator gnome-keyring signal-desktop \
     obsidian-bin libreoffice obs-studio kdenlive \
-    xournalpp localsend-bin spotify_player
+    xournalpp localsend-bin
 
   # Packages known to be flaky or having key signing issues are run one-by-one
-  for pkg in pinta typora zoom; do
+  for pkg in pinta typora spotify zoom; do
     yay -S --noconfirm --needed "$pkg" ||
       echo -e "\e[31mFailed to install $pkg. Continuing without!\e[0m"
   done
