4f56efe0e92e85907760654490e97117c75cffe6
David Heinemeier Hansson <david@hey.com>
2025-07-29 15:18:30 +0200

    Include the common JetBrains substitution

--- install/desktop/fonts.sh
+++ install/desktop/fonts.sh
@@ -3,5 +3,5 @@
 yay -S --noconfirm --needed ttf-font-awesome ttf-cascadia-mono-nerd ttf-ia-writer noto-fonts noto-fonts-emoji
 
 if [ -z "$OMARCHY_BARE" ]; then
-  yay -S --noconfirm --needed noto-fonts-cjk noto-fonts-extra
+  yay -S --noconfirm --needed ttf-jetbrains-mono noto-fonts-cjk noto-fonts-extra
 fi
