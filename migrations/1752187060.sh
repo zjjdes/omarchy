a98adc499a3535ec839d4af85dafd67cec780b1d
David Heinemeier Hansson <david@hey.com>
2025-07-30 22:47:34 +0200

    Fix migration

--- migrations/1752187060.sh
+++ migrations/1752187060.sh
@@ -4,10 +4,10 @@ if ! fc-list | grep -qi "CaskaydiaMono Nerd Font Propo"; then
   cd /tmp
   wget https://github.com/ryanoasis/nerd-fonts/releases/latest/download/CascadiaMono.zip
   unzip CascadiaMono.zip -d CascadiaFont
-  cp CascadiaFont/CaskaydiaMonoNerdFontPropo-Regular.ttf ~/.local/share/fonts
-  cp CascadiaFont/CaskaydiaMonoNerdFontPropo-Bold.ttf ~/.local/share/fonts
-  cp CascadiaFont/CaskaydiaMonoNerdFontPropo-Italic.ttf ~/.local/share/fonts
-  cp CascadiaFont/CaskaydiaMonoNerdFontPropo-BoldItalic.ttf ~/.local/share/fonts
+  cp CascadiaFont/CaskaydiaMonoNerdFontPropo-Regular.ttf ~/.local/share/fonts/
+  cp CascadiaFont/CaskaydiaMonoNerdFontPropo-Bold.ttf ~/.local/share/fonts/
+  cp CascadiaFont/CaskaydiaMonoNerdFontPropo-Italic.ttf ~/.local/share/fonts/
+  cp CascadiaFont/CaskaydiaMonoNerdFontPropo-BoldItalic.ttf ~/.local/share/fonts/
   rm -rf CascadiaMono.zip CascadiaFont
   fc-cache
   cd -
