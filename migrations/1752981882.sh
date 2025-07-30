f0b1dd286b93da40aa3737ba5806d7e462cdd6ba
David Heinemeier Hansson <david@hey.com>
2025-07-30 23:32:17 +0200

    Dropbox is now an optional setup

--- migrations/1752981882.sh
+++
@@ -1,5 +0,0 @@
-echo "Add missing libappindicator-gtk3 for Dropbox icon tray to work right"
-
-if ! pacman -Q libappindicator-gtk3 &>/dev/null; then
-  yay -S --noconfirm --needed libappindicator-gtk3
-fi
