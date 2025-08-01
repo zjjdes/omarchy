7bca5c1903eef4cc750c9a52d7c30c2893cc8925
David Heinemeier Hansson <david@hey.com>
2025-08-01 14:26:28 +0200

    Make Yaru-blue the default

--- install/desktop/theme.sh
+++ install/desktop/theme.sh
@@ -17,6 +17,7 @@ fi
 
 gsettings set org.gnome.desktop.interface gtk-theme "Adwaita-dark"
 gsettings set org.gnome.desktop.interface color-scheme "prefer-dark"
+gsettings set org.gnome.desktop.interface icon-theme "Yaru-blue"
 
 # Setup theme links
 mkdir -p ~/.config/omarchy/themes
