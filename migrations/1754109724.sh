4b027a9242b0e8be7267d7a04076bd830ff901fe
David Heinemeier Hansson <david@hey.com>
2025-08-03 06:47:05 -0400

    Move out of conditional in case user has already installed hyprsunset

--- migrations/1754109724.sh
+++ migrations/1754109724.sh
@@ -1,6 +1,6 @@
 echo "Add hyprsunset blue light filter"
 if ! command -v hyprsunset &>/dev/null; then
   yay -S --noconfirm --needed hyprsunset
-  omarchy-refresh-hyprsunset
 fi
 
+omarchy-refresh-hyprsunset
