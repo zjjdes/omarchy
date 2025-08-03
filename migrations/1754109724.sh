65ba23d0bbc063495ba2d1599ce2fe36a0865a1a
David Heinemeier Hansson <david@hey.com>
2025-08-03 10:45:39 +0200

    Extract omarchy-refresh-hyprsunset

--- migrations/1754109724.sh
+++ migrations/1754109724.sh
@@ -1,7 +1,6 @@
 echo "Add hyprsunset blue light filter"
 if ! command -v hyprsunset &>/dev/null; then
-    yay -S --noconfirm --needed hyprsunset
+  yay -S --noconfirm --needed hyprsunset
+  omarchy-refresh-hyprsunset
 fi
-cp -r ~/.local/share/omarchy/config/hypr/hyprsunset.conf ~/.config/hypr/
-pkill hyprsunset
-setsid uwsm app -- hyprsunset &>/dev/null &
+
